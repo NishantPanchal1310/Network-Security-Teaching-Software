900fdab6dc23a9c518f77f3c83d8902e4fc01df8
Panchal Nishant <nishantppanchal@gmail.com>
2020-08-26 23:53:20 +1000

    Finishing up the Menu_function

--- Menu_Function.py
+++ Menu_Function.py
@@ -1,6 +1,10 @@
-def menuFunction(optionList):
+def menuFunction(menuTitle, optionList):
+    if menuTitle != None:
+        print(menuTitle)
+    
     for r in range(0, len(optionList)):
-        print(f"[{r}], - {optionList[r]}")
+        print(f" [{r}], - {optionList[r]}")
+
     print("[99] - Exit")
 
     optionChoosen = input('Enter option: ')
