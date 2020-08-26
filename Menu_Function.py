7fa28cda9d0b74957dc4d2d016cd0317a2d0777d
Sukhman123 <58316794+Sukhman123@users.noreply.github.com>
2020-08-26 10:26:22 +1000

    Update Menu_Function.py

--- Menu_Function.py
+++ Menu_Function.py
@@ -1,1 +1,11 @@
-def menuFunction()
+def menuFunction():
+
+    for r in range(0, len(optionList)):
+        print(f"[{r}], - {optionList[r]}")
+        print()
+    print("[99] - Exit")
+
+    str(option) = input('Enter option: ')
+    if option == '99':
+        exit()
+        
