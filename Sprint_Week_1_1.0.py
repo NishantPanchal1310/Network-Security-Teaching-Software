d48e6a031a5f4b5e413441f1eff53811a9a64a4a
DC-88 <70185825+DC-88@users.noreply.github.com>
2020-08-26 10:09:10 +1000

    Update Sprint_Week_1_1.0.py

--- Sprint_Week_1_1.0.py
+++ Sprint_Week_1_1.0.py
@@ -3,41 +3,29 @@ import hashlib
 import base64
 import pickle
 
-#Create class of message
-class Message:
-    def __init__(self, text = " ", filename = "Not found", message_list = [], hashed_result = ""):
-        self.text = text 
-        self.filename = filename
-        self.message_list = message_list
-        self.hashed_result = hashed_result
     
-    #adds text into message_list directly
-    def addText(self, user_input):
-        self.message_list.append(user_input)
+#Hashes message with sha3_512      
+def hashText(text):
+    byte_obj = (str(text)).encode()
+    hash_obj = hashlib.sha3_512(byte_obj)
+    output = hash_obj.hexdigest()
+    return output
     
-    #Hashes message with md5
-    def hashMessage(self, text):
-        #convert to bytes
-        to_byte = text.encode()
-        #convert to hash
-        to_hash = hashlib.md5(to_byte)
-        #store hashed result as hex
-        self.hashed_result = to_hash.hexdigest()
-        return self.hashed_result
-    
-    #Reads message_list in a pickle file with a given name and returns message_list
-    def readFilePk(self, filename):
-        try 
-            pickle_in = open(str(filename),"rb")
-            self.message_list = pickle.load(pickle_in)
+#Reads pickle file with a given name and returns whatever is stored
+def readFilePk(filename):
+    try:
+        pickle_in = open(str(filename),"rb")
+        message_get = pickle.load(pickle_in)
+        pickle_in.close()
+        return message_get
+    except:
+        return "File not found"
 
-            pickle_in.close()
-            return self.message_list
-        except:
-            return "File not found"
+#save given text to a pickle file of a given name
+def saveFilePk(text, filename):
+    pickle_out = open(str(filename),"wb")
+    pickle.dump(text, pickle_out)
+    pickle_out.close()
+        
 
-    #save message_list to a pickle file of a given name
-    def saveFilePk(self, filename):
-        pickle_out = open(str(filename),"wb")
-        pickle.dump(self.message_list, pickle_out)
-        pickle_out.close()
+
