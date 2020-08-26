7989227e1fa66f6400e098c4f64e78ca16e3bc63
DC-88 <70185825+DC-88@users.noreply.github.com>
2020-08-26 10:11:43 +1000

    Update Module_Sprint_Week_1_1.0.py

--- Module_Sprint_Week_1_1.0.py
+++ Module_Sprint_Week_1_1.0.py
@@ -2,7 +2,30 @@
 import hashlib
 import base64
 import pickle
+from time import sleep
+from Clear_Terminal_Function import clearTerminal
 
+
+
+# Define the function to get a loading screen
+## redundantText can be set to None is nothing is wanted as the redundant text
+def loadingAnimation(redundantText, numberOfAnimationLoops):
+    
+    clearTerminal() # Clears the terminal 
+
+    for a in range(0, numberOfAnimationLoops, ): # Loops animation twice
+        loadingStr = "loading" # Resets the string to loading
+        
+        for b in range(0, 4): # Code is repeated four times
+            loadingStr += "." # An dot is added to the end of the string loading
+            
+            if redundantText != None: # Checks that redundantText is not None
+                print(redundantText) # Prints the redundantText
+            
+            print(loadingStr) # Prints the loading string
+            sleep(0.7) # Sleeps for 0.7 seconds
+            clearTerminal() # Clears the terminal
+            
     
 #Hashes message with sha3_512      
 def hashText(text):
