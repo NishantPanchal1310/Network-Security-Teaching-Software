67df3e86325251a3b3bf3560642a6e1e94b4d7a8
Panchal Nishant <nishantppanchal@gmail.com>
2020-09-08 09:30:04 +1000

    8/09/2020\

--- Prime_Number_Generator.py
+++ Prime_Number_Generator.py
@@ -1,24 +1,14 @@
 import math
 from Functions_Module_WK1 import saveFilePK
-<<<<<<< HEAD
  # Function to generator prime number
-=======
-
-#This function basically generates prime numbers for the RSA key generation
->>>>>>> 628a233996fb46692824cbab7fe34ff36a068a59
 def gen_prime(bits):
     primes = []
     
     #Creates numFrom and numTo, which are used in the for loop to test for prime numbers
     numFrom = int(2**((bits-1)/2))
     numTo = int(2**(bits/2))
-<<<<<<< HEAD
 
     # For a number number between numFrom and numTo, it checks if there is any divisible number. If not, it is added to the prime list.
-=======
-    
-    #Checks j in the range of numFrom and numTo and checks if it is divisible.
->>>>>>> 628a233996fb46692824cbab7fe34ff36a068a59
     for j in range(numFrom, numTo):
         for k in range(2, j):
             if (j % k) == 0:
@@ -30,11 +20,6 @@ def gen_prime(bits):
      
     # generates filename for prime numbers to be saved to.
     filename = "primes_" + str(bits) + "bits"     
-<<<<<<< HEAD
 
     # Save file
     saveFilePK(primes, filename)
-=======
-    #saves the list of prime numbers to a file 
-    saveFilePK(primes, filename)
->>>>>>> 628a233996fb46692824cbab7fe34ff36a068a59
